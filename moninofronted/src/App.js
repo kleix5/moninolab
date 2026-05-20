ad123c146a6c595629a6c02cf2adda2bdb1a52f9
Barbos Bos <isipkov3@gmail.com>
2026-05-20 03:55:05 +0300

    Основу сделал осталось ссылки нормально сделать

--- moninofronted/src/App.js
+++ moninofronted/src/App.js
@@ -1,11 +1,13 @@
 import { React,} from 'react';
 import Navbar from './Navbar.jsx';
+import Footer from './Footer.jsx';
 
 
 function App(){
   return (
     <div>
       <Navbar/>
+      <Footer></Footer>
     </div>
   )
 
