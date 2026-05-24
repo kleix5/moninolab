4b29083a8c55fd28e4358dbdbb047e7a358c92db
Barbos Bos <isipkov3@gmail.com>
2026-05-25 01:08:37 +0300

    Сделал sidebar

--- moninofronted/src/App.js
+++ moninofronted/src/App.js
@@ -9,10 +9,12 @@ import Sidebar from './Sidebar/Sidebar.jsx';
 
 function App(){
   return (
-    <div>
+    <div className="page-layout">
       <Navbar/>
-      <Sidebar/>
-
+      <div className="page-body">
+        <Sidebar/>
+        <div className="content">
+          <div className='product-grid'>
         <ProductCard
           image="./manul/стикер.png"
           title="Продукт 1"
@@ -164,8 +166,46 @@ function App(){
         price="??"
         rating="4.5"
         onAddToCart={() => alert('Добавлено в корзину!')}
-      />
-
+      /><ProductCard
+          image="./manul/стикер.png"
+          title="Продукт 1"
+          price="??"
+          rating="4.5"
+          onAddToCart={() => alert('Добавлено в корзину!')}
+        /><ProductCard
+          image="./manul/стикер.png"
+          title="Продукт 1"
+          price="??"
+          rating="4.5"
+          onAddToCart={() => alert('Добавлено в корзину!')}
+        /><ProductCard
+          image="./manul/стикер.png"
+          title="Продукт 1"
+          price="??"
+          rating="4.5"
+          onAddToCart={() => alert('Добавлено в корзину!')}
+        /><ProductCard
+          image="./manul/стикер.png"
+          title="Продукт 1"
+          price="??"
+          rating="4.5"
+          onAddToCart={() => alert('Добавлено в корзину!')}
+        /><ProductCard
+          image="./manul/стикер.png"
+          title="Продукт 1"
+          price="??"
+          rating="4.5"
+          onAddToCart={() => alert('Добавлено в корзину!')}
+        /><ProductCard
+          image="./manul/стикер.png"
+          title="Продукт 1"
+          price="??"
+          rating="4.5"
+          onAddToCart={() => alert('Добавлено в корзину!')}
+        />
+          </div>
+        </div>
+      </div>
       <Footer/>
     </div>
   )
